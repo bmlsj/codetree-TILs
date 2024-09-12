d7486bf0f0d7b50661fe9d707f0c7ecd8336e94e
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-09-12 10:42:16 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240912/황금비율 토스트/golden-toast.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int[][] map;
+	private static boolean[][] visited;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] split = in.readLine().split(" ");
+		int m = Integer.parseInt(split[1]);
+		
+		LinkedList<String> list = new LinkedList<>();
+		for (String s : in.readLine().split("")) {
+			list.add(s);
+		}
+		
+		ListIterator<String> it = list.listIterator(list.size());
+		for(int i = 0; i < m;i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			
+			String comments = st.nextToken();
+			if (comments.equals("L")) {
+				if (it.hasPrevious()) {
+					it.previous();
+				}
+			} else if (comments.equals("R")) {
+				if (it.hasNext()) {
+					it.next();
+				}
+			} else if (comments.equals("D")) {
+				it.next();
+				it.remove();
+			} else if (comments.equals("P")) {
+				it.add(st.nextToken());
+			}
+		}
+		
+		it = list.listIterator();
+		while(it.hasNext()) {
+			System.out.print(it.next());
+		}
+		
+	}
+
+}
