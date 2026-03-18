357e0074534274c221991d9c7433e661310d9d4a
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2026-03-19 00:57:22 +0900

    Title: 국영수 순이지, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260319/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Student implements Comparable<Student> {
+
+	String name;
+	int kor, eng, math;
+
+	Student(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		if (this.kor == o.kor)
+			return o.eng - this.eng;
+		else if (this.eng == o.eng)
+			return o.math - this.math;
+		else
+			return o.kor - this.kor;
+	}
+
+}
+
+public class M {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		List<Student> stud = new ArrayList<Student>();
+		for (int i = 0; i < N; i++) {
+			String[] split = br.readLine().split(" ");
+			String name = split[0];
+			int kor = Integer.parseInt(split[1]);
+			int eng = Integer.parseInt(split[2]);
+			int math = Integer.parseInt(split[3]);
+
+			stud.add(new Student(name, kor, eng, math));
+		}
+
+		Collections.sort(stud);
+
+		for (Student st : stud) {
+			System.out.println(st.name + " " + st.kor + " " + st.eng + " " + st.math);
+		}
+
+	}
+
+}
